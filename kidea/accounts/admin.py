e858947bbe5bbd02b9c678937d9d05c40849df26
IAmAngelLiu <b07705006@ntu.edu.tw>
2021-05-05 22:57:30 +0800

    add member and product database

--- kidea/accounts/admin.py
+++ kidea/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Member
+from .models import Product
+
+admin.site.register(Member)
+admin.site.register(Product)
+
+
+
--- kidea/accounts/models.py
+++ kidea/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null= True)
+	email = models.CharField(max_length=200, null=True)
+	address = models.CharField(max_length=200, null=True)
+	date_created = models.DateTimeField(auto_now_add=True)
+
+	def _str_(self):
+		return self.name
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=200, null=True)
+	price = models.FloatField(null= True)
+	description = models.CharField(max_length=500, null=True)
+	date_created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
