4449c861599fac45862bf3e4c476163f1167f4c0
dynamiteNing <45146727+dynamiteNing@users.noreply.github.com>
2021-05-24 22:18:24 +0800

    Update urls.py
    add cart.urls

--- kidea/kidea/urls.py
+++ kidea/kidea/urls.py
@@ -26,7 +26,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('', include('accounts.urls')),  
+    path('', include('accounts.urls')),  
+    path('', include('cart.urls')),
 ]
 
 
